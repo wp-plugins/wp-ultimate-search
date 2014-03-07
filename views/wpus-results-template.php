e96ea9672a825ca6026de9efad814cd204e0e3e5
sekatsim <sekatsim@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-07 22:50:15 +0000

    tagging version 1.5
    git-svn-id: https://plugins.svn.wordpress.org/wp-ultimate-search/tags/1.5@871497 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/wpus-results-template.php
+++ views/wpus-results-template.php
@@ -1,28 +1,30 @@
-<?php if ($results): ?>
 
-	<?php global $post; ?>
+<?php if ( $wpus_results->have_posts() ) : ?>
 	
-	<?php foreach ($results as $post): ?>
-		
-		<?php setup_postdata($post); ?>
+	<?php while ( $wpus_results->have_posts() ) : $wpus_results->the_post(); ?>
 
 		<div class="post" id="post-<?php the_ID(); ?>">
-		<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">
-		<?php the_title(); ?></a></h2>
+		<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
 			<small><?php // the_time('F jS, Y') ?> <!-- by <?php the_author() ?> --></small>
 			<div class="entry">
 				<?php the_excerpt(); ?>
 			</div>
-			<p class="postmetadata">Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  
-			<?php comments_popup_link('No Comments »', '1 Comment »', '% Comments »'); ?></p>
+			<p class="postmetadata">
+				Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  
+				<?php comments_popup_link('No Comments »', '1 Comment »', '% Comments »'); ?>
+			</p>
 		</div>
 		
-	<?php endforeach; ?>
+	<?php endwhile; ?>
 	
 	<?php if(wpus_option('clear_search')) { ?>
 		<a id="wpus-clear-search" class="<?php echo wpus_option('clear_search_class') ?>" href="#"><?php echo wpus_option('clear_search_text'); ?></a>
 	<?php } ?>
 	
+	<?php wp_reset_postdata(); ?>
+
 <?php else : ?>
+
 		<div class="wpus-no-results"><?php echo wpus_option('no_results_msg'); ?></div>
+
 <?php endif; ?>
